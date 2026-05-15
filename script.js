bfee8a69545946e032c213368b870c33c3573b66
mahakpichienslin <mahakpichien.slin@gmail.com>
2026-05-15 13:47:29 +0700

    fix mobile robot layout

--- script.js
+++ script.js
@@ -27,3 +27,39 @@ document.addEventListener("mousemove", (e) => {
 
   robotModel.setAttribute("camera-orbit", `${x}deg ${75 - y}deg 15m`);
 });
+
+const slider = document.querySelector(".screenshots-track");
+
+if (slider) {
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("dragging");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+
+  slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("dragging");
+  });
+
+  slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("dragging");
+  });
+
+  slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+
+    e.preventDefault();
+
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 0.8;
+
+    slider.scrollLeft = scrollLeft - walk;
+  });
+}
